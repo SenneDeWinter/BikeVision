de0936dc996c23638db29933361d5d3914eb2501
SenneDeWinter <senne.dewinter@student.kdg.be>
2023-04-11 13:49:25 +0200

    updated button styling and placement

--- messages.php
+++ messages.php
@@ -31,27 +31,6 @@ if (isset($_SESSION['logged_in'])) {
     <div class="text-2xl font-light">
         <br>
         <h1>Messages</h1>
-        <form action="logout.php">
-            <button type="submit" name="submit" class="
-                w-full
-                px-6
-                py-2.5
-                bg-blue-600
-                text-white
-                font-medium
-                text-xs
-                leading-tight
-                uppercase
-                rounded
-                shadow-md
-                hover:bg-blue-700 hover:shadow-lg
-                focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-                active:bg-blue-800 active:shadow-lg
-                transition
-                duration-150
-                ease-in-out" id="submit">Logout
-            </button>
-        </form>
     </div>
     <div class="flex flex-col">
         <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -119,6 +98,28 @@ if (isset($_SESSION['logged_in'])) {
             </div>
         </div>
     </div>
+    <br>
+    <form action="logout.php">
+            <button type="submit" name="submit" class="
+                w-auto
+                px-6
+                py-2.5
+                bg-blue-600
+                text-white
+                font-medium
+                text-xs
+                leading-tight
+                uppercase
+                rounded
+                shadow-md
+                hover:bg-blue-700 hover:shadow-lg
+                focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+                active:bg-blue-800 active:shadow-lg
+                transition
+                duration-150
+                ease-in-out" id="submit">Logout
+            </button>
+    </form>
 </div>
 </body>
 </html>
--- newsletter.php
+++ newsletter.php
@@ -36,6 +36,28 @@ if (isset($_SESSION['logged_in'])) {
             <p class="text-black-200 text-sm font-light"><?= $subscribers['email'];?></p>
         <?php endforeach; ?>    
     </div>
+    <br>
+    <form action="logout.php">
+            <button type="submit" name="submit" class="
+                w-auto
+                px-6
+                py-2.5
+                bg-blue-600
+                text-white
+                font-medium
+                text-xs
+                leading-tight
+                uppercase
+                rounded
+                shadow-md
+                hover:bg-blue-700 hover:shadow-lg
+                focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+                active:bg-blue-800 active:shadow-lg
+                transition
+                duration-150
+                ease-in-out" id="submit">Logout
+            </button>
+    </form>
 </div>
 </body>
 </html>
